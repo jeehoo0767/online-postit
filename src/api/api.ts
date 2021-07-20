cb85f0f16437456d5726dcf8de6b27b80421a1ff
jeehoo0767 <jeehoo0767@naver.com>
2021-07-20 12:25:17 +0900

    fix [api] 한가지 함수는 한가지 기능만 하도록 api요청 함수, storage접근 함수 분리

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,21 @@
 export const api = {
   getPostList() {
     return new Promise((resolve) => {
-      if ((localStorage.getItem('noteList') as string) === '[]' || localStorage.getItem('noteList') === null) {
-        return resolve([{ description: '', height: 250, id: 0, isFoldPost: false, isVisible: false, title: '', width: 250, x: 10, y: 10 }]);
+      if ((localStorage.getItem('noteList') as string) === '[]' || localStorage.getItem('noteList') === 'null') {
+        return resolve(setPostItems());
       }
       if (localStorage.getItem('noteList')) {
-        return resolve(JSON.parse(localStorage.getItem('noteList') as string));
+        return resolve(getLocalStorageItem());
       }
     });
   },
 };
 // 요청 시 noteList이름의 localStorage 아이템이 있다면 해당 객체를 리턴, 아니면 빈 배열을 리턴
+
+const getLocalStorageItem = () => {
+  return JSON.parse(localStorage.getItem('noteList') as string);
+};
+
+const setPostItems = () => {
+  return [{ description: '', height: 250, id: 0, isFoldPost: false, isVisible: false, title: '', width: 250, x: 10, y: 10 }];
+};
